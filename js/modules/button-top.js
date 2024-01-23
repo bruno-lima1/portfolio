a9a4d950a49d633eb83398b3302e150fdecda03e
Bruno César <brunocesar.tenorio1@gmail.com>
2024-01-23 10:08:34 -0300

    updating button top

--- js/modules/button-top.js
+++ js/modules/button-top.js
@@ -1,11 +1,11 @@
 export default function initButtonTop() {
   const button = document.querySelector("[data-button-top]");
   if (button) {
-    button.addEventListener("click", scrollToTop);
-    function scrollToTop() {
+    button.addEventListener("click", handleClick);
+    window.addEventListener("scroll", hideButton);
+    function handleClick() {
       scrollTo(0, 0);
     }
-    window.addEventListener("scroll", hideButton);
     function hideButton() {
       return scrollY > 10
         ? button.classList.add("active")
