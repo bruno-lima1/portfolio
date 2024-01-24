5d84f06e223fd95b15f285584cfd0afd1e7987bd
Bruno César <brunocesar.tenorio1@gmail.com>
2024-01-24 13:23:45 -0300

    adding menu mobile

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 import initSmoothScroll from "./modules/smooth-scroll.js";
 import initButtonTop from "./modules/button-top.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initOutsideClick from "./modules/outside-click.js";
 initSmoothScroll();
 initButtonTop();
+initMenuMobile();
+initOutsideClick();
 
 if (window.SimpleAnime) {
   new SimpleAnime();
