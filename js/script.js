ab26fa78819c022bc3bb0991aa3133e1b38a281a
Bruno César <brunocesar.tenorio1@gmail.com>
2024-04-04 16:51:12 -0300

    deleting back to top button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 import initSmoothScroll from "./modules/smooth-scroll.js";
-import initButtonTop from "./modules/button-top.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initOutsideClick from "./modules/outside-click.js";
 initSmoothScroll();
-initButtonTop();
 initMenuMobile();
 initOutsideClick();
 
