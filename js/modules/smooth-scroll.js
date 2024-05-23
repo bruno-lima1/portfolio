9bfdc20548d6aa57a738460abb37ac2aaecfc42a
Bruno César <brunocesar.tenorio1@gmail.com>
2024-05-23 15:00:11 -0300

    adding smooth scroll in title

--- js/modules/smooth-scroll.js
+++ js/modules/smooth-scroll.js
@@ -1,19 +1,26 @@
-export default function initSmoothScroll() {
-  const buttons = document.querySelectorAll("[data-smoothScroll] a");
-  if (buttons) {
-    buttons.forEach((button) => {
-      ["touchstart", "click"].forEach((userEvent) => {
-        button.addEventListener(userEvent, activeScroll);
-      });
-    });
-    function activeScroll(event) {
-      event.preventDefault();
-      const href = event.currentTarget.getAttribute("href");
-      const section = document.querySelector(href);
-      section.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
+export default class SmoothScroll {
+  constructor(buttons) {
+    this.buttons = document.querySelectorAll(buttons)
+    this.enableSmoothScroll = this.enableSmoothScroll.bind(this)
+  }
+  init() {
+    if (this.buttons.length) {
+      this.eventSmoothScroll()
     }
+    return this;  
+  }
+  eventSmoothScroll() {
+    this.buttons.forEach((button) => {
+      button.addEventListener("click", this.enableSmoothScroll)
+    })
+  }
+  enableSmoothScroll(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href)
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-import initSmoothScroll from "./modules/smooth-scroll.js";
+import SmoothScroll from "./modules/smooth-scroll.js"
+const smoothScroll = new SmoothScroll("[data-smoothScroll] a")
+smoothScroll.init()
+
+// import initSmoothScroll from "./modules/smooth-scroll.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initOutsideClick from "./modules/outside-click.js";
-initSmoothScroll();
+// initSmoothScroll();
 initMenuMobile();
 initOutsideClick();
 
